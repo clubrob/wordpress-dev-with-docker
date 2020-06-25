db69a8107577fbf5b522315e958b67d1d5929891
Rob Lindsey <rob@roblindsey.com>
2020-06-25 19:37:18 -0400

    added build step for plugin

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,27 @@ const {src, dest, series, watch} = require('gulp');
 const browser = require('browser-sync').create();
 const reload = browser.reload;
 
-const path = process.env.THEME_PATH;
+const themePath = process.env.THEME_PATH;
+const pluginPath = process.env.PLUGIN_PATH;
 
-function browserSyncInit() {
+function themeWatcher() {
   browser.init({
     port: 5500,
     proxy: 'localhost:8080'
   });
-  watch(`${path}/theme.css`).on('change', reload);
-  watch(`${path}/js/main.js`).on('change', reload);
-  watch(`${path}/**/*.php`).on('change', reload);
+  watch(`${themePath}/theme.css`).on('change', reload);
+  watch(`${themePath}/js/main.js`).on('change', reload);
+  watch(`${themePath}/**/*.php`).on('change', reload);
 }
 
-exports.serve = series(browserSyncInit);
+function pluginWatcher() {
+  browser.init({
+    port: 5500,
+    proxy: 'localhost:8080'
+  });
+  watch(`${pluginPath}/build/*.js`).on('change', reload);
+  watch(`${pluginPath}/**/*.php`).on('change', reload);
+}
+
+exports.serveThemes = series(themeWatcher);
+exports.servePlugins = series(pluginWatcher);
